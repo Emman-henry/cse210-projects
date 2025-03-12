c6188b5da2f76a4c6d938e06cfbad1b616e38c46
emman-henry <officialgr8henry@gmail.com>
2025-03-12 09:05:00 +0000

    Your message here

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Declare variables
+        string firstName, lastName;
+
+        // Ask for first name
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine(); // Capture user input
+
+        // Ask for last name
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine(); // Capture user input
+
+        // Output the result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
