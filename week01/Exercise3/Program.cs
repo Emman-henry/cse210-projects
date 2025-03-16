0dae22d42a41bb390f2593b7165202b8fdcdc784
emman-henry <officialgr8henry@gmail.com>
2025-03-16 17:58:36 +0000

    Your message here

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,14 +1,39 @@
 using System;
-using System.Globalization;
-using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int magic_number;
-        magic_number=[1,100];
-        magic_number=RandomNumberGenerator()
-        Console.WriteLine("pick a number");
+        // Create a random number generator
+        Random random = new Random();
+        
+        // Generate a magic number between 1 and 100
+        int magic_number = random.Next(1, 101);
+        
+        Console.WriteLine("What is your guess?");
+
+        // Read the user's guess
+        int guess = int.Parse(Console.ReadLine());
+        
+        // Loop until the user guesses the correct number
+        while (guess != magic_number)
+        {
+            // Check the guess
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            // Ask the user to guess again
+            Console.WriteLine("Try again! What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        }
+        
+        // When the user guesses correctly
+        Console.WriteLine("You just got the magic number!");
     }
 }
